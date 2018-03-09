99522a8955701a3349179d0d875a3ea17868117f
igor <igopko1996@gmail.com>
2018-03-09 20:57:48 +0200

    Add files via upload

--- src/app/first-component/first.component.ts
+++ src/app/first-component/first.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-first',
-    templateUrl:'.first.cpmponent.html',
-    styleUrls:'./first.component'
+    templateUrl:'./first.component.html',
+    styleUrls:['./first.component.css']
 })
 export class FirstComponent{
-    
+
 }
